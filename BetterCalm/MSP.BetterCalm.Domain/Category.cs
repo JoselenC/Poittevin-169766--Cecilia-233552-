e0062f6033bc59f344aeb3da807764f4e5f7e639
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-25 19:57:27 -0300

    Se crea la clase Category

---
+++ BetterCalm/MSP.BetterCalm.Domain/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace MSP.BetterCalm.Domain
+{
+    public class Category
+    {
+        public string Name { get; set; }
+    }
+}
--- BetterCalm/MSP.BetterCalm.Test/CategoryTest.cs
+++ BetterCalm/MSP.BetterCalm.Test/CategoryTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Domain;
 
 namespace MSP.BetterCalm.Test
 {
