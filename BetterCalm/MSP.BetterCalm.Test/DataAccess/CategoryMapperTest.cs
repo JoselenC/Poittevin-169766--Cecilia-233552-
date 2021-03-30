891cf0b6bba10a87be0d92a41dd844f5c6ed81f8
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 12:57:05 -0300

    Se crean los test para mappearlos objetos de la bdd al dominio

---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/CategoryMapperTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.BusinessLogic.Exceptions;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class CategoryMapperTest
+    {
+       
+        public  DataBaseRepository<Category, CategoryDto> Categories;
+        public  Category categoryTest;
+
+        [TestInitialize]
+        public  void TestFixtureSetup()
+        {
+            Categories = new DataBaseRepository<Category, CategoryDto>(new CategoryMapper());
+            CleanData();
+            categoryTest = new Category()
+            {
+                Name = "Dormir",
+            };
+            Categories.Add(categoryTest);
+            
+        }
+
+        [TestCleanup]
+        public void CleanData()
+        {
+            foreach (Category category in Categories.Get())
+            {
+                Categories.Delete(category);
+            }
+        }
+        
+        [TestMethod]
+        public void DomainToDtoTest()
+        {
+            Category categoryTest = new Category()
+            {
+                Name = "Yoga"
+            };
+            Categories.Add(categoryTest);
+            Category realCategory = Categories.Find(x => x.Name == "Yoga");
+            Assert.AreEqual(categoryTest, realCategory);
+            
+        }
+
+        [TestMethod]
+        public void DtoToDomainTest()
+        {
+            Category realCategory = Categories.Find(x => x.Name == "Dormir");
+            Assert.AreEqual(categoryTest, realCategory);
+        }
+        
+        
+    }
+}
---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/ProblematicMapperTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.BusinessLogic.Exceptions;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass()]
+    public class ProblematicMapperTest
+    {
+        public  DataBaseRepository<Problematic, ProblematicDto> Problematics;
+        public  Problematic problematicTest;
+
+        [TestInitialize]
+        public  void TestFixtureSetup()
+        {
+            Problematics = new DataBaseRepository<Problematic, ProblematicDto>(new ProblematicMapper());
+            CleanData();
+            problematicTest = new Problematic()
+            {
+                Name = "Dormir",
+            };
+            Problematics.Add(problematicTest);
+        }
+
+        [TestCleanup]
+        public void CleanData()
+        {
+            foreach (Problematic problematic in Problematics.Get())
+            {
+                Problematics.Delete(problematic);
+            }
+        }
+        
+        [TestMethod]
+        public void DomainToDtoTest()
+        {
+            Problematic problematicTest = new Problematic()
+            {
+                Name = "Estres"
+            };
+            Problematics.Add(problematicTest);
+            Problematic realProblematic = Problematics.Find(x => x.Name == "Estres");
+            Assert.AreEqual(problematicTest, realProblematic);
+        }
+
+        [TestMethod]
+        public void DtoToDomainTest()
+        {
+            Problematic realProblematic = Problematics.Find(x => x.Name == "Dormir");
+            Assert.AreEqual(problematicTest, realProblematic);
+        }
+     
+    }
+}
