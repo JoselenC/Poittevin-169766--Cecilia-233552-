7d14439f431d2d5b671e96ea06dfd58fea7fffc7
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 13:08:08 -0300

    Se crean excepciones

---
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/Exceptions/NoFindProblematicByName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MSP.BetterCalm.BusinessLogic.Exceptions
+{
+    public class NoFindProblematicByName: Exception
+    {
+        public NoFindProblematicByName() :
+            base("No find category by name")
+        { }
+    }
+}
--- BetterCalm/MSP.BetterCalm.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MSP.BetterCalm.DataAccess
-{
-    public class Class1
-    {
-    }
-}
