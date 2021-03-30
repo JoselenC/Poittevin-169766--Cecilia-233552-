14fc8cc6428a54ddf29ce0bd3aed62e06513f334
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 13:07:38 -0300

    Se crea el contextDB y se agrega la configuracion de entity

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/Migrations/20210330003012_BetterCalmDB.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MSP.BetterCalm.DataAccess.Migrations
+{
+    public partial class BetterCalmDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryDtoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryDtoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Problematics",
+                columns: table => new
+                {
+                    ProblematicDtoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problematics", x => x.ProblematicDtoID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Problematics");
+        }
+    }
+}
