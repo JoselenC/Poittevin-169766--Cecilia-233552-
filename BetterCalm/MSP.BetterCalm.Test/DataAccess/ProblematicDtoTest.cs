d2a11a77b3bf42d3e8760a79d9636ebfa84d28b3
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 12:53:29 -0300

    Se crea la clase problematicDtoTest, para guardar los objetos en la bdd

---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/ProblematicDtoTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class ProblematicDtoTest
+    {
+        [TestMethod]
+        public void SetGetProblematicDtoId()
+        {
+            ProblematicDto problematicDto = new ProblematicDto();
+            problematicDto.ProblematicDtoID = 1;
+            Assert.AreEqual(1, problematicDto.ProblematicDtoID);
+        }
+
+        [TestMethod]
+        public void SetGetProblematicName()
+        {
+            ProblematicDto problematicDto = new ProblematicDto();
+            problematicDto.Name = "food";
+            Assert.AreEqual("food", problematicDto.Name);
+        }
+        
+    }
+}
