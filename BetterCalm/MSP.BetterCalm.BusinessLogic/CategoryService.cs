fad5702b5db5221a3fb58ed638daa2ae765f1c4c
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 13:04:27 -0300

    Se crean las clases service de los objetos Category y Problematic

---
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using MSP.BetterCalm.BusinessLogic.Exceptions;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public class CategoryService
+    {
+        private ManagerRepository repository;
+
+        public CategoryService(ManagerRepository vRepository)
+        {
+            repository = vRepository;
+        }
+        
+        public List<Category> GetCategories()
+        {
+            return repository.Categories.Get();
+        }            
+
+        public Category GetCategoryByName(string name)
+        {
+            try
+            {
+                return repository.Categories.Find(x => x.IsSameCategoryName(name));
+            }
+            catch (ValueNotFound)
+            {
+                throw new NoFindCategoryByName();
+            }
+
+        }
+
+        public void SetCategory(Category category)
+        {
+            repository.Categories.Add(category);
+        }
+    }
+}
