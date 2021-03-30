668a0e89769f0f7fcc06c6f3f2b65bb53970df10
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 12:52:31 -0300

    Se crea la clase problematic, con los metodos para que pasen los test

---
+++ BetterCalm/MSP.BetterCalm.Domain/Problematic.cs
@@ -0,0 +1,20 @@
+﻿namespace MSP.BetterCalm.Domain
+{
+    public class Problematic
+    
+    {
+        public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null) return false;
+            if (obj.GetType() != GetType()) return false;
+            return Name == ((Problematic)obj).Name;
+        }
+
+        public bool IsSameProblematicName(string problematicName)
+        {
+            return Name == problematicName;
+        }
+    }
+}
