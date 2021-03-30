ce7e0d64e149ed4209b76173eb49f13d8516524d
Joselen <JC233552@fi365.ort.edu.uy>
2021-03-30 12:58:34 -0300

    Se crean las clases mapper para pasar los test, y la interfaz Imapper

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/Mappers/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public interface IMapper<D,T>
+    {
+        T DomainToDto(D obj, ContextDB context);
+
+        D DtoToDomain(T obj, ContextDB context);
+
+        void UpdateDtoObject<T, D>(T objToUpdate, D updatedObject, ContextDB context) where T : class;
+    }
+}
