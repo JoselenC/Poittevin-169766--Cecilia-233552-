64ea64f5bac651a98890cf7607f961779704c293
Joselen <JC233552@fi365.ort.edu.uy>
2021-04-03 12:48:13 -0300

    Se agregan los controladores de problematic y category

---
+++ BetterCalm/MSP.BetterCalm.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Category")]
+    public class CategoryController : ControllerBase
+    {
+
+        private ICategoryLogic categoryLogic;
+
+        public CategoryController(ICategoryLogic categoryLogic)
+        {
+            this.categoryLogic = categoryLogic;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Category> categories = this.categoryLogic.GetCategories();
+            return Ok(categories);
+        }
+        
+        [HttpGet("{Name}")]
+        public IActionResult GetCategoryByName([FromRoute]string Name)
+        {
+            Category categoryByName=categoryLogic.GetCategoryByName(Name);
+            return Ok(categoryByName);
+        }
+
+    }
+}
