b3731de17391e384db594b3d9c5895be9317f547
Joselen <JC233552@fi365.ort.edu.uy>
2021-04-05 18:41:49 -0300

    Se agrega la clase Playlist

---
+++ BetterCalm/MSP.BetterCalm.Domain/Playlist.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class Playlist
+    {
+        public List<Category> Categories {get; set; }
+
+        public List<Song> Songs {get; set; }
+        
+        public string Name {get; set; }
+        
+        public string UrlImage {get; set; }
+        
+        public string Description {get; set; }
+
+        public bool IsSamePlaylistName(string name)
+        {
+            return Name == name;
+        }
+        
+        public bool IsSameCategoryName(string name)
+        {
+            foreach (var category in Categories)
+            {
+                if (category.Name == name)
+                    return true;
+            }
+            return false;
+        }
+        
+        public bool IsSameSongName(string name)
+        {
+            foreach (var song in Songs)
+            {
+                if (song.Name == name)
+                    return true;
+            }
+            return false;
+        }
+        
+        
+    }
+}
