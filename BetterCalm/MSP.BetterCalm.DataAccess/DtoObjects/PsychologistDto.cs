4b7ffb7e211ef61dfccfd9fa2309b0ff4bfa75ab
Juan Pablo <juanpaps03@gmail.com>
2021-04-06 20:03:11 -0300

    Agrego Patient-Psychologist-User Dtos

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/DtoObjects/PsychologistDto.cs
@@ -0,0 +1,8 @@
+namespace MSP.BetterCalm.DataAccess
+{
+    public class PsychologistDto: UserDto
+    {
+        public int PsychologistDtoId { get; set; }
+        public string Address { get; set; }
+    }
+}
