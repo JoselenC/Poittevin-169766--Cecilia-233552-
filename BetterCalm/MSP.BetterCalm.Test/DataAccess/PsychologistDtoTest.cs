b03c236542045a1a6eb26d2e6e26f64c405cc811
Juan Pablo <juanpaps03@gmail.com>
2021-04-06 20:02:25 -0300

    Agrego Patient-Psychologist Dto tests

---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/PsychologistDtoTest.cs
@@ -0,0 +1,79 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class PsychologistDtoTest
+    {
+        [TestMethod]
+        public void GetSetSongId()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                PsychologistDtoId = 1
+            };
+            Assert.AreEqual(1, psychologist.PsychologistDtoId);
+        }
+        
+        [TestMethod]
+        public void GetSetAddress()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                Address = "House 1234"
+            };
+            Assert.AreEqual("House 1234", psychologist.Address);
+        }
+        
+        [TestMethod]
+        public void GetSetUserId()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                UserDtoId = 11
+            };
+            Assert.AreEqual(11, psychologist.UserDtoId);
+        }
+        
+        [TestMethod]
+        public void GetSetName()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                Name = "Pedro"
+            };
+            Assert.AreEqual("Pedro", psychologist.Name);
+        }
+        
+        [TestMethod]
+        public void GetSetLastName()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                LastName = "Rodriguez"
+            };
+            Assert.AreEqual("Rodriguez", psychologist.LastName);
+        }
+        
+        [TestMethod]
+        public void GetSetEmail()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                Email = "my.email@gmail.com"
+            };
+            Assert.AreEqual("my.email@gmail.com", psychologist.Email);
+        }
+        
+        [TestMethod]
+        public void GetSetPassword()
+        {
+            PsychologistDto psychologist = new PsychologistDto
+            {
+                Password = "StrongHashedPass"
+            };
+            Assert.AreEqual("StrongHashedPass", psychologist.Password);
+        }
+    }
+}
