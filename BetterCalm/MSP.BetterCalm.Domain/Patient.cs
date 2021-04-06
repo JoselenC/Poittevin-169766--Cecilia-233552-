c69f9920276f1ad62b7aa51a15431c852e3edd72
Juan Pablo <juanpaps03@gmail.com>
2021-04-06 20:00:32 -0300

    Agrego Patient Psychologist y User clases de dominio

---
+++ BetterCalm/MSP.BetterCalm.Domain/Patient.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class Patient: User
+    {
+        
+        public string Cellphone { get; set; }
+        public DateTime BirthDay { get; set; }
+        protected bool Equals(Patient other)
+        {
+            return Cellphone == other.Cellphone && BirthDay.Equals(other.BirthDay);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Patient) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Cellphone, BirthDay);
+        }
+
+    }
+}
