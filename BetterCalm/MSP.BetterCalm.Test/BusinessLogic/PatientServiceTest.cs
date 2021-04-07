c4da71917c0aa566a42f0f5703fd4e5701bc89a5
Juan Pablo <juanpaps03@gmail.com>
2021-04-07 09:36:54 -0300

    Agrego Patient service y controller Test

---
+++ BetterCalm/MSP.BetterCalm.Test/BusinessLogic/PatientServiceTest.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class PatientServiceTest
+    {
+        private Mock<ManagerPatientRepository> repoMock;
+        private Mock<IRepository<Patient>> patientMock;
+        private PatientService service;
+
+        [TestInitialize]
+        public void TestFixtureSetup()
+        {
+            repoMock = new Mock<ManagerPatientRepository>();
+            patientMock = new Mock<IRepository<Patient>>();
+            repoMock.Object.Patientes = patientMock.Object;
+            service = new PatientService(repoMock.Object);
+        }
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            Patient patient = new Patient()
+            {
+                Name = "Patient1"
+            };
+            List<Patient> patients = new List<Patient>
+            {
+                patient
+            };
+            patientMock.Setup(
+                x => x.Get()
+            ).Returns(patients);
+            List<Patient> actualPatients = service.GetPatients();
+            CollectionAssert.AreEqual(patients, actualPatients);
+            patientMock.VerifyAll();
+        }
+
+    }
+}
