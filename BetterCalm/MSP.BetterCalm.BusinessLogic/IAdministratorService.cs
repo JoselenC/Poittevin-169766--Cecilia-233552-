5e3f4f08c9cadb052058c5b3f92f79f2c71cfda0
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:40:29 -0300

    Agrego admin Service e Interface

---
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/IAdministratorService.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public interface IAdministratorService
+    {
+        List<Administrator> GetAdministrators();
+    }
+}
