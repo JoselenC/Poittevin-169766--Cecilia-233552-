71d41fe10286d55b805d21026927fd8733701237
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:32:55 -0300

    Agrego IPsychologistService

---
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/IPsychologistService.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public interface IPsychologistService
+    {
+        List<Psychologist> GetPsychologists();
+    }
+}
