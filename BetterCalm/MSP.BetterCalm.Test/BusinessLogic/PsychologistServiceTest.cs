054460840bbea5c58a510ffda7da6c906ab01b4b
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:31:46 -0300

    Agrego Psychologist Service Test

---
+++ BetterCalm/MSP.BetterCalm.Test/BusinessLogic/PsychologistServiceTest.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class PsychologistServiceTest
+    {
+        
+        private Mock<ManagerPsychologistRepository> repoMock;
+        private Mock<IRepository<Psychologist>> psychologistMock;
+        private PsychologistService service;
+
+        [TestInitialize]
+        public void TestFixtureSetup()
+        {
+            repoMock = new Mock<ManagerPsychologistRepository>();
+            psychologistMock = new Mock<IRepository<Psychologist>>();
+            repoMock.Object.Psychologists = psychologistMock.Object;
+            service = new PsychologistService(repoMock.Object);
+        }
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            Psychologist psychologist = new Psychologist()
+            {
+                Name = "Psychologist1"
+            };
+            List<Psychologist> psychologists = new List<Psychologist>
+            {
+                psychologist
+            };
+            psychologistMock.Setup(
+                x => x.Get()
+            ).Returns(psychologists);
+            List<Psychologist> actualPsychologists = service.GetPsychologists();
+            CollectionAssert.AreEqual(psychologists, actualPsychologists);
+            psychologistMock.VerifyAll();
+        }
+
+        
+    }
+}
