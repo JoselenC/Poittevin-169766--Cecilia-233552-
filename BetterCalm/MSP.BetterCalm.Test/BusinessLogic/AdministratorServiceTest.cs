557fd36c287a52a0f998ca3ba466fc3abda4887a
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:40:19 -0300

    Agrego Admin Service Test

---
+++ BetterCalm/MSP.BetterCalm.Test/BusinessLogic/AdministratorServiceTest.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    public class AdministratorServiceTest
+    {
+        private Mock<ManagerAdministratorRepository> repoMock;
+        private Mock<IRepository<Administrator>> administratorMock;
+        private IAdministratorService service;
+
+        [TestInitialize]
+        public void TestFixtureSetup()
+        {
+            repoMock = new Mock<ManagerAdministratorRepository>();
+            administratorMock = new Mock<IRepository<Administrator>>();
+            repoMock.Object.Administrators = administratorMock.Object;
+            service = new AdministratorService(repoMock.Object);
+        }
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            Administrator administrator = new Administrator()
+            {
+                Name = "Administrator1"
+            };
+            List<Administrator> administrators = new List<Administrator>
+            {
+                administrator
+            };
+            administratorMock.Setup(
+                x => x.Get()
+            ).Returns(administrators);
+            List<Administrator> actualAdministrators = service.GetAdministrators();
+            CollectionAssert.AreEqual(administrators, actualAdministrators);
+            administratorMock.VerifyAll();
+        }
+    }
+}
