a77e858f2d3488ace42556a4b2f4266ce10ff274
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:38:28 -0300

    Agrego Administrator Mapper Test y agrego Equals de Admin

--- BetterCalm/MSP.BetterCalm.Domain/Administrator.cs
+++ BetterCalm/MSP.BetterCalm.Domain/Administrator.cs
@@ -1,7 +1,31 @@
+using System;
+
 namespace MSP.BetterCalm.Domain
 {
     public class Administrator: User
     {
+        protected bool Equals(Administrator other)
+        {
+            return 
+                Email == other.Email && 
+                Password == other.Password &&
+                Name == other.Name &&
+                LastName == other.LastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Administrator) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Email, Password);
+        }
+
         public string Email { get; set; }
         public string Password { get; set; }
     }
---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/Repositories/AdministratorMapperTest.cs
@@ -0,0 +1,60 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class AdministratorMapperTest
+    {
+        private DbContextOptions<ContextDB> options;
+        public  DataBaseRepository<Administrator, AdministratorDto> RepoAdministrators;
+        public  Administrator administratorTest;
+
+        [TestInitialize]
+        public  void TestFixtureSetup()
+        {
+            options = new DbContextOptionsBuilder<ContextDB>().UseInMemoryDatabase(databaseName: "BetterCalmDB").Options;
+            ContextDB context = new ContextDB(this.options);
+            RepoAdministrators = new DataBaseRepository<Administrator, AdministratorDto>(new AdministratorMapper(), context.Administrators, context);
+            administratorTest = new Administrator()
+            {
+                Name = "Juan",
+                LastName = "Poittevin",
+                Email = "email@test.com",
+                Password = "pass21"
+            };
+            RepoAdministrators.Add(administratorTest);
+        }
+        
+        [TestMethod]
+        public void DomainToDtoTest()
+        {
+            Administrator administratorTest = new Administrator()
+            {
+                Name = "Jose"
+            };
+            RepoAdministrators.Add(administratorTest);
+            Administrator actualAdministrator = RepoAdministrators.Find(x => x.Name == "Jose");
+            Assert.AreEqual(administratorTest, actualAdministrator);
+        }
+
+        [TestMethod]
+        public void DtoToDomainTest()
+        {
+            Administrator actualAdministrator = RepoAdministrators.Find(x => x.Name == "Juan");
+            Assert.AreEqual(this.administratorTest, actualAdministrator);
+        }
+     
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Administrator actualAdministrator = RepoAdministrators.Find(x => x.Name == "Juan");
+            actualAdministrator.Name = "JuanUpdated";
+            Administrator updatedAdministrator = RepoAdministrators.Update(administratorTest, actualAdministrator);
+            Assert.AreEqual(actualAdministrator, updatedAdministrator);
+        }
+    }
+}
