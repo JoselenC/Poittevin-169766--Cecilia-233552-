83854f19ddbe649625c88818b837d58c447460d9
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:32:19 -0300

    AGrego  Psychologist Controller

---
+++ BetterCalm/MSP.BetterCalm.WebAPI/Controllers/PsychologistController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.WebAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("api/Psychologist")]
+    public class PsychologistController: ControllerBase
+    {
+        
+        private IPsychologistService psychologistService;
+
+        public PsychologistController(IPsychologistService psychologistService)
+        {
+            this.psychologistService = psychologistService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Psychologist> patientes = psychologistService.GetPsychologists();
+            return Ok(patientes);
+        }
+    }
+}
