d5f47de10c2361879be6b3f8bb8fe4e5057bd46f
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:21:36 -0300

    Agrego Administrador Dto y Actualizo UserDto

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/DtoObjects/AdministratorDto.cs
@@ -0,0 +1,9 @@
+namespace MSP.BetterCalm.DataAccess
+{
+    public class AdministratorDto: UserDto
+    {
+        public int AdministratorDtoId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
