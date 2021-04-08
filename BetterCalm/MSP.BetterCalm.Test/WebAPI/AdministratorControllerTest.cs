6e880cdb52f714ceed7486eafe8d52ac967bb26c
Juan Pablo <juanpaps03@gmail.com>
2021-04-08 20:40:41 -0300

    Agrego  Admin Controller Test

---
+++ BetterCalm/MSP.BetterCalm.Test/WebAPI/AdministratorControllerTest.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.WebAPI.Controllers;
+
+namespace MSP.BetterCalm.Test.WebAPI
+{
+    [TestClass]
+    public class AdministratorControllerTest
+    {
+        private Mock<IAdministratorService> mockAdministratorService;
+        private AdministratorController administratorController ;
+        private List<Administrator> administrators;
+        private Administrator administrator;
+        
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            mockAdministratorService = new Mock<IAdministratorService>(MockBehavior.Strict);
+            administratorController = new AdministratorController(mockAdministratorService.Object);
+            administrators = new List<Administrator>();
+            administrator = new Administrator()
+            {
+                Name = "Juan"
+            };
+            administrators.Add(administrator);
+        }
+
+        [TestMethod]
+        public void TestGetAllAdministrators()
+        {
+            mockAdministratorService.Setup(x => x.GetAdministrators()).Returns(administrators);
+            var result = administratorController.GetAll();
+            var okResult = result as OkObjectResult;
+            var realAdministrators = okResult.Value;
+            Assert.AreEqual(realAdministrators, administrators);
+        }
+
+    }
+}
