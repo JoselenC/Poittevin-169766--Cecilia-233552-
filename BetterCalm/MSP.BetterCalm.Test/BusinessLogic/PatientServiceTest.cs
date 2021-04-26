5d1a29dc1feff528d68e44a80aba3b81f5a068a9
Juan Pablo <juanpaps03@gmail.com>
2021-04-25 21:55:39 -0300

    Agrego PatientService Test

--- BetterCalm/MSP.BetterCalm.Test/BusinessLogic/PatientServiceTest.cs
+++ BetterCalm/MSP.BetterCalm.Test/BusinessLogic/PatientServiceTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -12,6 +13,9 @@ namespace MSP.BetterCalm.Test
         private Mock<ManagerPatientRepository> repoMock;
         private Mock<IRepository<Patient>> patientMock;
         private PatientService service;
+        
+        private Mock<ManagerPsychologistRepository> psyRepoMock;
+        private Mock<IRepository<Psychologist>> psychologistMock;
 
         [TestInitialize]
         public void TestFixtureSetup()
@@ -19,7 +23,13 @@ namespace MSP.BetterCalm.Test
             repoMock = new Mock<ManagerPatientRepository>();
             patientMock = new Mock<IRepository<Patient>>();
             repoMock.Object.Patients = patientMock.Object;
-            service = new PatientService(repoMock.Object);
+            
+            psyRepoMock = new Mock<ManagerPsychologistRepository>();
+            psychologistMock = new Mock<IRepository<Psychologist>>();
+            psyRepoMock.Object.Psychologists = psychologistMock.Object;
+            
+            service = new PatientService(repoMock.Object, psyRepoMock.Object);
+
         }
 
         [TestMethod]
@@ -53,6 +63,37 @@ namespace MSP.BetterCalm.Test
             );
             service.AddPatient(patient);
             patientMock.VerifyAll();
+        }
+        
+        
+        [TestMethod]
+        public void TestScheduleNewMeeting()
+        {
+            Patient patient = new Patient()
+            {
+                Name = "Patient1"
+            };
+            Psychologist psychologist = new Psychologist()
+            {
+                Name = "psychologist1"
+            };
+            Problematic problematic = new Problematic()
+            {
+                Name = "problem1"
+            };
+            Meeting expectedMeeting = new Meeting()
+            {
+                DateTime = DateTime.Today,
+                Patient = patient,
+                Psychologist = psychologist
+            };
+            psychologistMock.Setup(
+                x => 
+                    x.Find(It.IsAny<Predicate<Psychologist>>())
+            ).Returns(psychologist);
+            Meeting actualMeeting = service.ScheduleNewMeeting(patient, problematic);
+            Assert.AreEqual(expectedMeeting, actualMeeting);
+            psychologistMock.VerifyAll();
         }
 
     }
