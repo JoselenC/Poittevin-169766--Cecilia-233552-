45fc40ac44dd082c8e80a9b7551e9312b6fe89f1
Joselen <JC233552@fi365.ort.edu.uy>
2021-04-29 22:00:49 -0300

    Se agregan nuevas exceptions

---
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/Exceptions/ObjectWasNotDeleted.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSP.BetterCalm.BusinessLogic.Exceptions
+{
+    public class ObjectWasNotDeleted : Exception
+    {
+        public ObjectWasNotDeleted() :
+            base("The id of the object to delete does not exist")
+        {
+        }
+    }
+}
