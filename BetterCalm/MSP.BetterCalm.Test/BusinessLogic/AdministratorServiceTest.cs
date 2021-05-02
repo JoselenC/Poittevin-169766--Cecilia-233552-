8659f388013f37a3c371ef1521a76a0d06a7eb80
Juan Pablo <juanpaps03@gmail.com>
2021-05-02 19:04:37 -0300

    Agrego tests de nuevos endpoints de administrator

--- BetterCalm/MSP.BetterCalm.Test/BusinessLogic/AdministratorServiceTest.cs
+++ BetterCalm/MSP.BetterCalm.Test/BusinessLogic/AdministratorServiceTest.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.BusinessLogic.Exceptions;
 using MSP.BetterCalm.Domain;
 
 namespace MSP.BetterCalm.Test
 {
+    [TestClass]
     public class AdministratorServiceTest
     {
         private Mock<ManagerAdministratorRepository> repoMock;
@@ -53,5 +56,88 @@ namespace MSP.BetterCalm.Test
             service.AddAdministrator(admin);
             administratorMock.VerifyAll();
         }
+        
+        
+        [TestMethod]
+        public void TestGetAdministratorById()
+        {
+            Administrator administrator = new Administrator()
+            {
+                Name = "administrator1",
+                AdministratorId = 1
+            };
+            administratorMock.Setup(
+                x => x.Find(It.IsAny<Predicate<Administrator>>())
+            ).Returns(administrator);
+            service.GetAdministratorsById(administrator.AdministratorId);
+            administratorMock.VerifyAll();
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundAdministrator))]
+        public void TestGetAdministratorByIdNotFound()
+        {
+
+            administratorMock.Setup(
+                x => x.Find(It.IsAny<Predicate<Administrator>>())
+            ).Throws(new KeyNotFoundException());
+            service.GetAdministratorsById(1);
+        }
+
+        [TestMethod]
+        public void TestUpdateAdministrator()
+        {
+            Administrator OldAdministrator = new Administrator()
+            {
+                AdministratorId = 2,
+                Name = "Administrator1"
+            };
+            Administrator NewAdministrator = new Administrator()
+            {
+                AdministratorId = 2,
+                Name = "Administrator32"
+            };
+            administratorMock.Setup(
+                x => x.Find(It.IsAny<Predicate<Administrator>>())
+            ).Returns(OldAdministrator);
+            administratorMock.Setup(
+                x => x.Update(OldAdministrator, NewAdministrator)
+            ).Returns(NewAdministrator);
+            Administrator realUpdated = service.UpdateAdministrator(NewAdministrator, OldAdministrator.AdministratorId);
+            Assert.AreEqual(NewAdministrator, realUpdated);
+            administratorMock.VerifyAll();
+        }
+        
+
+        [TestMethod]
+        public void TestDeleteAdministrator()
+        {
+            Administrator administrator = new Administrator()
+            {
+                Name = "administrator1",
+                AdministratorId = 1
+            };
+            administratorMock.Setup(
+                x => x.Delete(administrator)
+            );
+            administratorMock.Setup(
+                x => x.Find(It.IsAny<Predicate<Administrator>>())
+            ).Returns(administrator);
+            service.DeleteAdministratorById(administrator.AdministratorId);
+            administratorMock.VerifyAll();
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundAdministrator))]
+        public void TestDeleteAdministratorNotFound()
+        {
+
+            administratorMock.Setup(
+                x => x.Find(It.IsAny<Predicate<Administrator>>())
+            ).Throws(new KeyNotFoundException());
+            service.DeleteAdministratorById(1);
+        }
+        
+        
     }
 }
