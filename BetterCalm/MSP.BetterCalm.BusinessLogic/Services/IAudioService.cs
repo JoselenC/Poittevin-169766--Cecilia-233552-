0e1c6db1bbf4ded190fedd77521e781f74da6cf8
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-03 09:42:35 -0300

    Validaciones nuevas en audioService y refactor sobre los metodos de esta

--- BetterCalm/MSP.BetterCalm.BusinessLogic/Services/IAudioService.cs
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/Services/IAudioService.cs
@@ -6,12 +6,11 @@ namespace MSP.BetterCalm.BusinessLogic
     public interface IAudioService
     {
         public List<Audio> GetAudios();
-        public void AddAudio(Audio audio);
+        public Audio SetAudio(Audio audio);
         public List<Audio> GetAudiosByName(string audioName);
         public List<Audio> GetAudiosByAuthor(string authorName);
         public List<Audio> GetAudiosByCategoryName(string categoryName);
         public void DeleteAudio(int id);
-        void DeleteAudio(List<Audio> playlistAudio);
         public Audio GetAudioById(int id);
         public void UpdateAudioById(int id, Audio audioUpdated);
     }
