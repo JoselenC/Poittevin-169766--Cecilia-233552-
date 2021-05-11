d6d033cc588f5c0e33bebcbff41a5f2be2f012e0
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-11 17:51:51 -0300

    Se agregan las clases VideoDto y videoDtoTest

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/DtoObjects/VideoDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class VideoDto
+    {
+        public int VideoDtoID { get; set; }
+        
+        public string Name {get; set; }
+        
+        public double Duration {get; set; }
+        
+        public string CreatorName {get; set; }
+        
+        public string UrlArchive {get; set; }
+        
+        public ICollection<PlaylistVideoDto> PlaylistVideosDto { get; set; }
+        
+        public ICollection<VideoCategoryDto> VideosCategoriesDto { get; set; }
+    }
+}
---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/Dtos/VideoDtoTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class VideoDtoTest
+    {
+        
+        [TestMethod]
+        public void GetSetVideoId()
+        {
+            VideoDto Video = new VideoDto();
+            Video.VideoDtoID = 1;
+            Assert.AreEqual(1, Video.VideoDtoID);
+        }
+        
+        [TestMethod]
+        public void GetSetVideoName()
+        {
+            string VideoName = "Let it be";
+            VideoDto Video = new VideoDto();
+            Video.Name = "Let it be";
+            string getVideoName = Video.Name;
+            Assert.AreEqual(VideoName, getVideoName);
+        }
+        
+        [TestMethod]
+        public void GetSetVideoUrlArchive()
+        {
+            string UrlArchiveName = "UrlArchiveName";
+            VideoDto Video = new VideoDto();
+            Video.UrlArchive = "UrlArchiveName";
+            string getVideoUrlArchive = Video.UrlArchive;
+            Assert.AreEqual(UrlArchiveName, getVideoUrlArchive);
+        }
+
+        [TestMethod]
+        public void GetSetVideoDuration()
+        {
+            double duration = 23 ;
+            VideoDto Video = new VideoDto();
+            Video.Duration = 23;
+            double getVideoDuration = Video.Duration;
+            Assert.AreEqual(duration, getVideoDuration);
+        }
+        
+        [TestMethod]
+        public void GetSetVideoCreatorName()
+        {
+            string CreatorName = "Paul McCartney";
+            VideoDto Video = new VideoDto();
+            Video.CreatorName = CreatorName;
+            string getCreatorName= Video.CreatorName;
+            Assert.AreEqual(CreatorName, getCreatorName);
+        }
+        
+        [TestMethod]
+        public void GetSetPlaylistVideoDto()
+        {
+            VideoDto video = new VideoDto();
+            video.PlaylistVideosDto = new List<PlaylistVideoDto>();
+            ICollection<PlaylistVideoDto> getPlaylistVideo= video.PlaylistVideosDto;
+            CollectionAssert.AreEqual(getPlaylistVideo.ToList(), new List<PlaylistVideoDto>());
+        }
+    }
+}
