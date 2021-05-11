65a1ae5802a9ab0fdad5b3682ffc59655a082050
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-11 17:35:39 -0300

    Se agregan las clases de las relaciones VideoCategroy y PlaylistVideo

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/DtoObjects/VideoCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MSP.BetterCalm.DataAccess
+{
+    public class VideoCategoryDto
+    {
+        public int VideoID { get; set; }
+        
+        public VideoDto VideoDto{ get; set; }
+        
+        public int CategoryID { get; set; }
+
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
