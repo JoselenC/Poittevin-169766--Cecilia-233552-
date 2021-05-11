2bfa464039c19a0c33e2f1c5819cc9ef9d2162d3
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-11 17:37:14 -0300

    Se crea la interfaz IVideoService para la inyeccion de dependencias

---
+++ BetterCalm/MSP.BetterCalm.BusinessLogic/Services/IVideoService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public interface IVideoService 
+    {
+        public List<Video> GetVideos();
+        public Video SetVideo(Video video);
+        public List<Video> GetVideosByName(string videoName);
+        public List<Video> GetVideosByAuthor(string authorName);
+        public List<Video> GetVideosByCategoryName(string categoryName);
+        public void DeleteVideo(int id);
+        public Video GetVideoById(int id);
+        public void UpdateVideoById(int id, Video videoUpdated);
+    }
+}
