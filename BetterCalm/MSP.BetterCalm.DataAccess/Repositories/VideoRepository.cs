ec6aac557e5b0ba924d736d3e016a8a3c8a9f69d
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-11 17:38:39 -0300

    Se crea el repository de video

---
+++ BetterCalm/MSP.BetterCalm.DataAccess/Repositories/VideoRepository.cs
@@ -0,0 +1,13 @@
+﻿using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class VideoRepository:ManagerVideoRepository
+    {
+        public VideoRepository(IMapper<Video, VideoDto> mapper, ContextDB context)
+        {
+            Videos = new DataBaseRepository<Video, VideoDto>(mapper, context.Videos, context);
+        }
+    }
+}
---
+++ BetterCalm/MSP.BetterCalm.Test/DataAccess/Repositories/VideoRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class VideoRepositoryTest
+    {
+        [TestMethod]
+        public void VideoRepositoryCreationCategoriesTypeTest()
+        {
+            VideoRepository VideoRepository = new VideoRepository(new VideoMapper(),new ContextDB());
+            Assert.IsInstanceOfType(VideoRepository.Videos, typeof(DataBaseRepository<Video, VideoDto>));
+        }
+    }
+}
