e7366a861dd3b5491ca6041c1566a9e543d4546f
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-23 11:43:44 -0300

    Se agrega el audio model

---
+++ BetterCalm/Frontend/src/app/models/Audio.ts
@@ -0,0 +1,15 @@
+﻿export class Audio {
+  id: number;
+  name: string;
+  authorName: string;
+  urlAudio: string;
+  urlImage: string;
+
+  constructor(id:number,name:string,authorName:string,urlAudio:string,urlImage:string){
+    this.name = name;
+    this.id=id;
+    this.authorName=authorName;
+    this.urlAudio=urlAudio;
+    this.urlImage=urlImage
+  }
+}
