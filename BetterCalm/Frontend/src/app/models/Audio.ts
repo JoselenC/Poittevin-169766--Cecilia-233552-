cb093d03c9cc3d6fb4b3bf2561885e6832598099
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-26 16:25:02 -0300

    Se modifica el model audio

--- BetterCalm/Frontend/src/app/models/Audio.ts
+++ BetterCalm/Frontend/src/app/models/Audio.ts
@@ -1,15 +1,22 @@
-﻿export class Audio {
-  id: number;
+﻿import {Category} from "./Category";
+
+export class Audio {
+  id!:number;
   name: string;
   authorName: string;
   urlAudio: string;
   urlImage: string;
+  categories:Array<Category>;
+  duration:string;
 
-  constructor(id:number,name:string,authorName:string,urlAudio:string,urlImage:string){
-    this.name = name;
+  constructor(id:number,name:string,authorName:string,urlAudio:string,urlImage:string,duration:string){
     this.id=id;
+    this.categories=new Array<Category>()
+    this.name = name;
     this.authorName=authorName;
     this.urlAudio=urlAudio;
-    this.urlImage=urlImage
+    this.urlImage=urlImage;
+    this.duration=duration
   }
+
 }
