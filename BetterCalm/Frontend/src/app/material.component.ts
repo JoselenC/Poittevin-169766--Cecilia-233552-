a82cd3eb911e95a604391d8458fe394120425719
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-26 16:26:13 -0300

    Se agrega el material components

---
+++ BetterCalm/Frontend/src/app/material.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from '@angular/core';
+
+/** @title Form field appearance variants */
+@Component({
+  selector: 'app.component',
+  templateUrl: 'app.component.html',
+})
+export class FormFieldAppearanceExample {}
