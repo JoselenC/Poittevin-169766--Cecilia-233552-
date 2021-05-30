eb468c85be29c479c8ec7f34c1ccc612166bc9d0
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-30 20:24:20 -0300

    Se agrega el model de playlist

---
+++ BetterCalm/Frontend/src/app/models/Playlist.ts
@@ -0,0 +1,21 @@
+﻿import {Category} from "./Category";
+import {Audio} from "./Audio";
+
+export class Playlist {
+  id!:number;
+  name: string;
+  urlImage: string;
+  categories:Array<Category>;
+  audios: Array<Audio>;
+  description:string;
+
+  constructor(id:number,name:string,urlImage:string,description:string){
+    this.id=id;
+    this.categories=new Array<Category>();
+    this.audios= new Array<Audio>();
+    this.name = name;
+    this.urlImage=urlImage;
+    this.description=description
+  }
+
+}
