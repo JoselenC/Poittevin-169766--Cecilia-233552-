fde9bb71e0eb48c57fcb215a11f2c4f2a73269bc
Joselen <JC233552@fi365.ort.edu.uy>
2021-05-30 20:22:34 -0300

    Se agregan los components de get playlists

---
+++ BetterCalm/Frontend/src/app/playlistComponents/get-playlists/get-playlists.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Playlist} from "../../models/Playlist";
+import {PlaylistService} from "../../services/Playlist/playlist.service";
+
+
+@Component({
+  selector: 'app-get-playlists',
+  templateUrl: './get-playlists.component.html',
+  styleUrls: ['./get-playlists.component.css']
+})
+export class GetPlaylistsComponent implements OnInit {
+
+  public playlists:Playlist[]=[]
+
+  constructor(private playlistService:PlaylistService) { }
+
+  ngOnInit(): void {
+    this.playlistService.getAll()
+      .subscribe(
+        ((data: Array<Playlist>) => this.result(data)),
+        ((error: any) => alert(error.message))
+      );
+  }
+  private result(data: Array<Playlist>): void {
+    this.playlists = data;
+  }
+}
