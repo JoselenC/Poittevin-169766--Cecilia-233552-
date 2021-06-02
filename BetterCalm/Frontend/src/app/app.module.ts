4a9777992748207a752f8819dea0d4b4a6d1235a
Juan Pablo <juanpaps03@gmail.com>
2021-06-01 21:05:44 -0300

    Agrego playlist y audio al menu

--- BetterCalm/Frontend/src/app/app.module.ts
+++ BetterCalm/Frontend/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {CategoryModule} from "./categoryComponents/category.module";
-import {HttpClientModule} from "@angular/common/http";
-import {AddAudioModule} from "./audioComponents/add-audio/add-audio.module";
-import {MatSliderModule} from "@angular/material/slider";
-import {FormFieldAppearanceExample} from "./material.component";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {GetAudiosModule} from "./audioComponents/get-audios/get-audios.module";
-import {DeleteAudioModule} from "./audioComponents/delete-audio/delete-audio.module";
-import {UpdateAudioModule} from "./audioComponents/update-audio/update-audio.module";
-import {LayoutModule} from './layout/layout.module';
 import {RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatSliderModule} from '@angular/material/slider';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormFieldAppearanceExample} from './material.component';
+
+import {LayoutModule} from './layout/layout.module';
+import { AppComponent } from './app.component';
+import {CategoryModule} from './categoryComponents/category.module';
+import {AddAudioModule} from './audioComponents/add-audio/add-audio.module';
+import {GetAudiosModule} from './audioComponents/get-audios/get-audios.module';
+import {DeleteAudioModule} from './audioComponents/delete-audio/delete-audio.module';
+import {UpdateAudioModule} from './audioComponents/update-audio/update-audio.module';
 
 import { PatientModule } from './patient/patient.module';
 import {PsychologistModule} from './psychologist/psychologist.module';
+import {GetPlaylistsModule} from './playlistComponents/get-playlists/get-playlists.module';
+import {AddPlaylistModule} from './playlistComponents/add-playlist/add-playlist.module';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/patients', // TODO: Redireccionar a playlist como pagina de inicio.
+    redirectTo: '/playlists', // TODO: Redireccionar a playlist como pagina de inicio.
     pathMatch: 'full'
   }
 ];
@@ -32,19 +34,27 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
-    LayoutModule,
-    PatientModule,
-    PsychologistModule
-    AppRoutingModule,
-    CategoryModule,
-    HttpClientModule,
-    AddAudioModule,
-    MatSliderModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    MatSliderModule,
+    LayoutModule,
+    // Patient
+    PatientModule,
+    // Psychologist
+    PsychologistModule,
+    // Category
+    CategoryModule,
+    // Audio
+    GetAudiosModule,
+    AddAudioModule,
     GetAudiosModule,
     DeleteAudioModule,
-    UpdateAudioModule
+    UpdateAudioModule,
+    // Playlist
+    GetPlaylistsModule,
+    AddPlaylistModule,
+    // Router
+    RouterModule.forRoot(routes),
   ],
   exports: [
     RouterModule
--- BetterCalm/Frontend/src/app/playlistComponents/add-playlist/add-playlist.module.ts
+++ BetterCalm/Frontend/src/app/playlistComponents/add-playlist/add-playlist.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AddPlaylistComponent} from "./add-playlist.component";
-import {HttpClientModule} from "@angular/common/http";
-import {MaterialComponentsModule} from "../../material.module";
-import {FormsModule} from "@angular/forms";
-import {MatOptionModule} from "@angular/material/core";
-import {CategoryModule} from "../../categoryComponents/category.module";
-import {GetAudiosModule} from "../../audioComponents/get-audios/get-audios.module";
+import {AddPlaylistComponent} from './add-playlist.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MaterialComponentsModule} from '../../material.module';
+import {FormsModule} from '@angular/forms';
+import {MatOptionModule} from '@angular/material/core';
+import {CategoryModule} from '../../categoryComponents/category.module';
+import {GetAudiosModule} from '../../audioComponents/get-audios/get-audios.module';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from '../../layout/layout.component';
 
-
+const routes: Routes = [
+  {
+    path: 'add-playlist',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: AddPlaylistComponent },
+    ]
+  },
+];
 
 @NgModule({
   declarations: [AddPlaylistComponent],
-  exports:[AddPlaylistComponent],
   imports: [
     CommonModule,
     HttpClientModule,
@@ -20,7 +29,11 @@ import {GetAudiosModule} from "../../audioComponents/get-audios/get-audios.modul
     FormsModule,
     MatOptionModule,
     CategoryModule,
-    GetAudiosModule
-  ]
+    GetAudiosModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
 })
 export class AddPlaylistModule { }
