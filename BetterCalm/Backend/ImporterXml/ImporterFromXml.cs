8fc87631ecc5f1f599146f6b5ee40dd6279c6ce0
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 11:27:32 -0300

    Se crea un importador, que importa desde xml

---
+++ BetterCalm/Backend/ImporterXml/ImporterFromXml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.Importer;
+
+namespace ImporterXml
+{
+    public class ImporterFromXml:IImporter
+    {
+
+        public string GetImporterName()
+        {
+            return "Xml";
+        }
+
+        public List<Content> ImportContent(string path)
+        {
+            List<Content> contents = new List<Content>();
+            var xmlReader = XmlReader.Create(path);
+            var xmlSerializer = new XmlSerializer(typeof(Content));
+            Content contentParse = (Content)xmlSerializer.Deserialize(xmlReader);
+            contents.Add(contentParse);
+            xmlReader.Close();
+            return contents;
+        }
+
+        public List<Parameter> GetParameters()
+        {
+            return new List<Parameter> 
+            {
+                new Parameter {
+                    Name = "Path",
+                    Type  = "string"
+                }
+            };
+        }
+    }
+}
---
+++ BetterCalm/Backend/MSP.BetterCalm.Test/ImportFromXml/ImporterFromXmlTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ImporterXml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.Importer;
+using MSP.BetterCalm.WebAPI.Dtos;
+
+namespace MSP.BetterCalm.Test.ImportFromXml
+{
+    [TestClass]
+    public class ImporterFromXmlTest
+    {
+
+        [TestMethod]
+        public void GetImporterNameTest()
+        {
+            IImporter importer = new ImporterFromXml();
+            string name = importer.GetImporterName();
+            Assert.AreEqual("Xml",name);
+        }
+        
+        [TestMethod]
+        public void GetParametersTest()
+        {
+            IImporter importer = new ImporterFromXml();
+            List<Parameter> parametersImporter = importer.GetParameters();
+            Parameter parameter = new Parameter() {Name = "Path", Type = "string"};
+            List<Parameter> parameters = new List<Parameter>() {parameter};
+            CollectionAssert.AreEqual(parameters,parametersImporter);
+        }
+        
+        [TestMethod]
+        public void ImportContentTest()
+        {
+            IImporter importer = new ImporterFromXml();
+            string path = @"../../../../MSP.BetterCalm.WebAPI/Parser/example.Xml";
+            List<Content> parametersImporter = importer.ImportContent(path);
+            CollectionAssert.AreEqual(parametersImporter,parametersImporter);
+        }
+    }
+}
