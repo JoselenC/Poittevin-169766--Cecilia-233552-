b5f27bf004065bac6d0c2220483c58bdc618456d
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 11:36:54 -0300

    Se agrega la clase Import

---
+++ BetterCalm/Backend/MSP.BetterCalm.Importer/Import.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MSP.BetterCalm.Importer
+{
+    public class Import
+    {
+        public string Name { get; set; }
+        public List<Parameter> Parameters { get; set; }
+        
+        public string Path { get; set; }
+
+    }
+}
