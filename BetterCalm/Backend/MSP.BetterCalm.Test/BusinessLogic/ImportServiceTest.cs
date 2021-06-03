ae94dc22e3bf3ab95544bf288e31ddae1315126b
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 12:35:44 -0300

    Se crea la clase ImportServiceTest

---
+++ BetterCalm/Backend/MSP.BetterCalm.Test/BusinessLogic/ImportServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.BusinessLogic.Managers;
+using MSP.BetterCalm.BusinessLogic.Services;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.Importer;
+
+namespace MSP.BetterCalm.Test.BusinessLogic
+{
+    [TestClass]
+    public class ImportServiceTest
+    {
+        
+        private Mock<ManagerContentRepository> _repoMock;
+        private Mock<IRepository<Content>> _contentsMock;
+        private ContentService _contentservice;
+        private ImportService _importService;
+
+        [TestInitialize]
+        public void TestFixtureSetup()
+        {
+            _repoMock = new Mock<ManagerContentRepository>();
+            _contentsMock = new Mock<IRepository<Content>>();
+            _repoMock.Object.Contents =  _contentsMock.Object;
+            _contentservice = new ContentService(_repoMock.Object);
+            _importService = new ImportService(_contentservice);
+            _importService._path=@"../../../../MSP.BetterCalm.WebAPI/Parser/";
+        }
+        
+        [TestMethod]
+        public void GetImportersNameTest()
+        {
+            List<string> importersNames= _importService.GetImportersName();
+           List<string> importers = new List<string>() {"Json", "Xml"};
+           CollectionAssert.AreEqual(importers,importersNames);
+        }
+        
+        [TestMethod]
+        public void GetParametersTest()
+        {
+            List<Parameter> importersNames= _importService.GetParameters();
+            List<Parameter> importers = new List<Parameter>() {new Parameter(){Name="Path", Type="string"}};
+            CollectionAssert.AreEqual(importers,importersNames);
+        }
+
+        [TestMethod]
+        public void ImportContentTest()
+        {
+            Import import = new Import()
+            {
+                Name = "Json",
+                Parameters = new List<Parameter>() {new Parameter() {Name = "Path", Type = "string"}},
+                Path = "../MSP.BetterCalm.WebAPI/Parser/"
+            };
+            List<Content> contents=_importService.ImportContent(import);
+            
+        }
+    }
+}
