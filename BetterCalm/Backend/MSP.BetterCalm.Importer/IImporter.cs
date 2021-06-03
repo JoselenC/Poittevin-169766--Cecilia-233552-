c19a1ad066b68671b347087cb8bfadd08a7e523b
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 09:49:01 -0300

    Se define y agrega el contrato de los importadores

---
+++ BetterCalm/Backend/MSP.BetterCalm.Importer/IImporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Importer
+{
+    public interface IImporter
+    {
+        string GetImporterName();
+        List<Content> ImportContent(string path);
+        List<Parameter> GetParameters();
+    }
+}
