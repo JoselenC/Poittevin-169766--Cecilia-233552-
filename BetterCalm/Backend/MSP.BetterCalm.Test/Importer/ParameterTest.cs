63365237106f386d29a75617a0a100982952a323
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 11:44:04 -0300

    Se agrega la clase parameterTest, que define la estructura de parameter

---
+++ BetterCalm/Backend/MSP.BetterCalm.Test/Importer/ParameterTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Importer;
+
+namespace MSP.BetterCalm.Test.Importer
+{
+    [TestClass]
+    public class ParameterTest
+    {
+
+        [TestMethod]
+        public void GetSetName()
+        {
+            string ParameterName = "Path";
+            Parameter Parameter = new Parameter();
+            Parameter.Name = "Path";
+            string getParameterName = Parameter.Name;
+            Assert.AreEqual(ParameterName, getParameterName);
+        }
+        
+        [TestMethod]
+        public void GetSetType()
+        {
+            string parameterType = "string";
+            Parameter parameter = new Parameter();
+            parameter.Type = "string";
+            string getParameterType = parameter.Type;
+            Assert.AreEqual(parameterType, getParameterType);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(InvalidType), "")]
+        public void GetSetInvalidType()
+        {
+            Parameter parameter = new Parameter();
+            parameter.Type = "aa";
+        }
+        
+    }
+}
