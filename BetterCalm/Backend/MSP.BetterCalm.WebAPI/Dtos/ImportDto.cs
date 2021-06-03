c597e0ace0554cee24fd38da6cd556f5b277dc84
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 12:48:22 -0300

    Se crea la clase importDto para usar en el controller de importar

---
+++ BetterCalm/Backend/MSP.BetterCalm.WebAPI/Dtos/ImportDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MSP.BetterCalm.Importer;
+
+
+namespace MSP.BetterCalm.WebAPI.Dtos
+{
+    public class ImportDto
+    {
+        public string Name { get; set; }
+        
+        public string Path { get; set; }
+    }
+}
