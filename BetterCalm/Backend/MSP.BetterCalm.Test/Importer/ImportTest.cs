a0e641d590551858df8ac068bda3e2b67f4eaeca
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-03 11:36:37 -0300

    Se agrega la clase ImportTest, que define la estructura de un import

---
+++ BetterCalm/Backend/MSP.BetterCalm.Test/Importer/ImportTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Importer;
+
+namespace MSP.BetterCalm.Test.Importer
+{
+    [TestClass]
+    public class ImportTest
+    {
+
+        [TestMethod]
+        public void GetSetName()
+        {
+            string importName = "json";
+            Import import = new Import();
+            import.Name = "json";
+            string getImportName = import.Name;
+            Assert.AreEqual(importName, getImportName);
+        }
+        
+        [TestMethod]
+        public void GetSetPath()
+        {
+            string pathName = "Path";
+            Import path = new Import();
+            path.Path = "Path";
+            string getImportPath = path.Path;
+            Assert.AreEqual(pathName, getImportPath);
+        }
+        
+        [TestMethod]
+        public void GetSetParameters()
+        {
+            Import path = new Import();
+            path.Parameters = new List<Parameter>();
+            List<Parameter> getpParameters = path.Parameters;
+            CollectionAssert.AreEqual(getpParameters, getpParameters);
+        }
+    }
+}
