9b4a0141e91cdcb4335683281419ecb6967a197e
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-05 17:56:17 -0300

    Se agregan atributos en playlist

--- BetterCalm/Frontend/src/app/models/Playlist.ts
+++ BetterCalm/Frontend/src/app/models/Playlist.ts
@@ -6,16 +6,16 @@ export class Playlist {
   name: string;
   urlImage: string;
   categories: Array<Category>;
-  audios: Array<Content>;
+  contents: Array<Content>;
   description: string;
 
   constructor(id: number, name: string , urlImage: string , description: string){
     this.id = id;
     this.categories = new Array<Category>();
-    this.audios = new Array<Content>();
+    this.contents = new Array<Content>();
     this.name = name;
     this.urlImage = urlImage;
-    this.description = description
+    this.description = description;
   }
 
 }
