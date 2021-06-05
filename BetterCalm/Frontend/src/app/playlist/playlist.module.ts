e69b97b1de3d3234595265aa79c86dba636242c2
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-05 17:55:58 -0300

    Se mieven todos los modules de playlist a uno

---
+++ BetterCalm/Frontend/src/app/playlist/playlist.module.ts
@@ -0,0 +1,87 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from '../layout/layout.component';
+import {GetPlaylistsComponent} from './get-playlists/get-playlists.component';
+import {LayoutModule} from '../layout/layout.module';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {AddPlaylistComponent } from './add-playlist/add-playlist.component';
+import {PlaylistDetailComponent} from './playlist-detail/playlist-detail.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {FlexModule} from '@angular/flex-layout';
+import {CategoryModule} from '../category/category.module';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {UpdatePlaylistComponent} from './update-playlist/update-playlist.component';
+
+const routes: Routes = [
+  {
+    path: 'playlists',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: GetPlaylistsComponent },
+    ]
+  },
+  {
+    path: 'playlists/:playlistId',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: PlaylistDetailComponent },
+    ]
+  },
+  {
+    path: 'add-playlist',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: AddPlaylistComponent },
+    ]
+  },
+  {
+    path: 'update-playlist',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: UpdatePlaylistComponent },
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [
+    GetPlaylistsComponent,
+    PlaylistDetailComponent,
+    AddPlaylistComponent,
+    UpdatePlaylistComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule.forChild(routes),
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    FlexModule,
+    FormsModule,
+    CategoryModule,
+    MatTooltipModule,
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class PlaylistModule { }
+
