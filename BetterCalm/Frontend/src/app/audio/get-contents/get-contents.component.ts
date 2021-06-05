50fa94b67a32915547ae4db1b20d98b4554367e2
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-05 17:50:35 -0300

    Se mueve la opcion delete para get

--- BetterCalm/Frontend/src/app/audio/get-contents/get-contents.component.ts
+++ BetterCalm/Frontend/src/app/audio/get-contents/get-contents.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ContentService} from '../../services/content/content.service';
 import {Content} from '../../models/Content';
+import {Router} from '@angular/router';
+import {ContentService} from '../../services/content/content.service';
+
 
 @Component({
   selector: 'app-get-contents',
@@ -11,8 +13,10 @@ export class GetContentsComponent implements OnInit {
 
   public contents: Content[] = [];
 
-  constructor(private contentService: ContentService) {
-  }
+  constructor(
+    private contentService: ContentService,
+    private router: Router
+  ) {  }
 
   ngOnInit(): void {
     this.contentService.getAll()
@@ -26,5 +30,14 @@ export class GetContentsComponent implements OnInit {
     this.contents = data;
   }
 
+  navigateTo(contentId: number): void {
+    this.router.navigate(['contents', contentId]);
+  }
 
+  delete(id: number): void {
+    this.contentService.delete(id).subscribe(
+      (error: any) => alert(error)
+    );
+    this.contents = this.contents.filter(p => p.id !== id);
+  }
 }
