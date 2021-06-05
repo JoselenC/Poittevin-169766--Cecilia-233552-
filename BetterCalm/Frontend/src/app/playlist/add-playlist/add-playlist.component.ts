4089209fd50587811d3bfe3e2b481feeba436bec
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-05 17:55:43 -0300

    Refactor sobre add playlist

--- BetterCalm/Frontend/src/app/playlist/add-playlist/add-playlist.component.ts
+++ BetterCalm/Frontend/src/app/playlist/add-playlist/add-playlist.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Playlist} from '../../models/Playlist';
-import {Router} from '@angular/router';
 import {PlaylistService} from '../../services/playlist/playlist.service';
+import {Router} from '@angular/router';
+import {Category} from '../../models/Category';
+import {CategoryService} from '../../services/category/category.service';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-add-playlist',
@@ -9,36 +12,56 @@ import {PlaylistService} from '../../services/playlist/playlist.service';
   styleUrls: ['./add-playlist.component.css']
 })
 export class AddPlaylistComponent implements OnInit {
-
   constructor(
-  private servicePlaylist: PlaylistService,
-  private router: Router
-  ){ }
+    private servicePlaylist: PlaylistService,
+    private serviceCategory: CategoryService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   id = 0;
   name = '';
+  authorName = '';
+  urlPlaylist = '';
   urlImage = '';
-  description = '';
+  duration = '';
+  data = Playlist;
+  categories: Array<string> = [
+    'Musica'
+  ];
+  cat = new FormControl();
+  catGroup ?: FormGroup;
 
   ngOnInit(): void {
+    this.initFormCategories();
+  }
+
+  initFormCategories(): void {
+    this.catGroup = this.formBuilder.group({
+      categoriesToString: this.cat
+    });
   }
 
 
-  addPlaylist(): void{
-    const playlist = new Playlist(
+  addPlaylist(): void {
+    const playlist = new Playlist (
       this.id,
       this.name,
-      this.urlImage,
-      this.description
+      this.cat.value.map((x: any) => (new Category(0, x))),
     );
-    this.servicePlaylist.add(playlist).subscribe(
+    this.servicePlaylist.newPlaylist(playlist).subscribe(
       (data: Playlist) => this.result(data),
-      (error: any) => alert(error)
+      (error: any) => {
+        console.log(error);
+        alert(error);
+      }
     );
-  }
-  // tslint:disable-next-line:typedef
-  private result(data: Playlist) {
-    this.router.navigate(['/playlist']);
   }
 
+  // tslint:disable-next-line:typedef
+  private result(data: Playlist) {
+    this.router.navigate(['/playlists']);
+  }
 }
+
+
