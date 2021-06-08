65f4b346670698322d1059881caab841ad542134
Joselen <JC233552@fi365.ort.edu.uy>
2021-06-08 13:07:23 -0300

    Se crea el objeto contentModel como objeto intermedio para importara

---
+++ BetterCalm/Backend/MSP.BetterCalm.Importer/Models/ContentModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Importer.Models
+{
+    public class ContentModel
+    {
+        
+        public string Name {get; set; }
+      
+        public string Duration { get; set; }
+        
+        public string CreatorName {get; set; }
+        
+        public string UrlImage {get; set; }  
+        
+        public string UrlArchive {get; set; }
+        
+        public string Type { get; set; }
+        
+        public List<Category> Categories { get; set; }
+        
+        public List<PlaylistModel> Playlists { get; set; }
+        
+        
+    
+       
+    }
+}
