f0a2f2de59a7e27f443525c5d3bb517c1b7f3adc
Juan Pablo <juanpaps03@gmail.com>
2021-06-12 00:53:26 -0300

    Agrego voucher dto test

---
+++ BetterCalm/Backend/MSP.BetterCalm.Test/DataAccess/Dtos/VoucherDtoTest.cs
@@ -0,0 +1,125 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess.DtoObjects;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.Test
+{
+    [TestClass]
+    public class VoucherDtoTest
+    {
+        [TestMethod]
+        public void GetSetPatient()
+        {
+            PatientDto patient = new PatientDto()
+            {
+                Name = "Test1"
+            };
+            VoucherDto voucher = new VoucherDto()
+            {
+                Patient = patient
+            };
+            Assert.AreEqual(voucher.Patient, patient);
+        }
+        
+        [TestMethod]
+        public void GetSetVoucherId()
+        {
+            VoucherDto voucher = new VoucherDto()
+            {
+                VoucherId = 1
+            };
+            Assert.AreEqual(voucher.VoucherId, 1);
+        }
+        
+        [TestMethod]
+        public void GetSetPsychologist()
+        {
+            PsychologistDto psychologist = new PsychologistDto()
+            {
+                Name = "Test1"
+            };
+            VoucherDto voucher = new VoucherDto()
+            {
+                Psychologist = psychologist
+            };
+            Assert.AreEqual(voucher.Psychologist, psychologist);
+        }
+        
+        [TestMethod]
+        public void GetSetMeetingAmount()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                MeetingsAmount = 3
+            };
+            Assert.AreEqual(voucher.MeetingsAmount, 3);
+        }
+        
+        [TestMethod]
+        public void GetSetTotalStatusApprove()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                Status = Status.Approved
+            };
+            Assert.AreEqual(voucher.Status, Status.Approved);
+        }
+        
+        [TestMethod]
+        public void GetSetTotalStatusRejected()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                Status = Status.Rejected
+            };
+            Assert.AreEqual(voucher.Status, Status.Rejected);
+        }
+        
+        [TestMethod]
+        public void GetSetTotalStatusPending()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                Status = Status.Pending
+            };
+            Assert.AreEqual(voucher.Status, Status.Pending);
+        }
+        
+        [TestMethod]
+        public void GetSetDiscountLow()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                Discount = Discounts.Low
+            };
+            Assert.AreEqual(voucher.Discount, Discounts.Low);
+        }
+        [TestMethod]
+        public void GetSetDiscountMedium()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                Discount = Discounts.Medium
+            };
+            Assert.AreEqual(voucher.Discount, Discounts.Medium);
+        }
+        
+        [TestMethod]
+        public void GetSetDiscountLarge()
+        {
+
+            VoucherDto voucher = new VoucherDto()
+            {
+                Discount = Discounts.Large
+            };
+            Assert.AreEqual(voucher.Discount, Discounts.Large);
+        }
+        
+    }
+}
